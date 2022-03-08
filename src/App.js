0723afa2d86d9ca0d5816376b8a6cc4e73163dbe
Mario Malitesta <mario.malitesta@gmail.com>
2022-03-08 18:14:31 +0100

    gttt

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks,setTasks] = useState(
+    [
+      {id: 1,
+      text: 'Hello1',
+      date: '1March',
+      reminder: true,
+      },
+      {id: 2,
+      text: 'Hello2',
+      date: '2March',
+      reminder: true,
+      },
+      {id: 3,
+      text: 'Hello3',
+      date: '3March',
+      reminder: false,
+      }
+    
+    ]
+  )
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div className="container">
+      <Header />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
